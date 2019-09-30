cccc1df5a280f7d0b14426eab75a7bd1d9a7eab7
Chris Fleming <me@chrisfleming.org>
2019-09-30 12:11:37 +0100

    Style sidebars

--- bbpress/my-templates/peacemaker/right-sidebar.php
+++ bbpress/my-templates/peacemaker/right-sidebar.php
@@ -2,80 +2,58 @@
 
 <ul>
 <h2><?php _e('RSS Feeds'); ?></h2>
-<p align = "left">
-<a href = "http://citycyclingedinburgh.info/bbpress/rss.php"  target="new"><img src="http://s.wordpress.com/wp-includes/images/rss.png" border = "0">posts</a><br>
-<a href = "http://citycyclingedinburgh.info/bbpress/rss.php?topics=1"  target="new"><img src="http://s.wordpress.com/wp-includes/images/rss.png" border = "0">topics</a>
-</p>
+<li><a href = "http://citycyclingedinburgh.info/bbpress/rss.php"  target="new"><img src="http://s.wordpress.com/wp-includes/images/rss.png" border = "0"> posts</a>
+<li><a href = "http://citycyclingedinburgh.info/bbpress/rss.php?topics=1"  target="new"><img src="http://s.wordpress.com/wp-includes/images/rss.png" border = "0"> topics</a>
+</ul>
+
 <ul>
 <h2><?php _e('Links'); ?></h2>
-
 <br>
 New site for people thinking of cycling to work<br>
 
-<a href="http://cyclefridays.wordpress.com" target="new">
+<li><a href="http://cyclefridays.wordpress.com" target="new">
 <img src="https://farm1.staticflickr.com/328/20246738209_8cef7e42d6_m_d.jpg" width="213" style="padding: 0px">
-</a><br>
+</a></li>
 
 
 
-<a href="http://pedalonparliament.org" target="new">
+<li><a href="http://pedalonparliament.org" target="new">
 <!--<img src="/images/pop/pop-2015_213x301.jpg" width="213" height="301" style="padding: 0px">-->
-Pedal on Parliament</a><br>
+Pedal on Parliament</a></li>
 
-<a href="http://citycyclingglasgow.com" target="new">citycyclingglasgow</a><br>
-<p><a href="http://citycycling.co.uk" target="new">.citycycling</a>
+<li><a href="http://cyclingedinburgh.wordpress.com">cyclingedinburgh (archived)</a></li>
+<li><a href="http://spokes.org.uk" target="new">Spokes</a> (Lothian Cycle Campaign)</li>
+<li><a href="http://www.cycling-edinburgh.org.uk" target="new">Cycling-Edinburgh</a> - details of local rides
+<li><a href="http://www.ctclothiansandborders.uk/" target="new">CTC Lothians</a> - events and useful info</li>
+<li><a href="http://www.ed.ac.uk/schools-departments/transport/cycling" target="new">University of Edinburgh's cycling pages</a> - Lots of useful info for staff and students</li>
 
-<br><a href="http://cyclingedinburgh.info">cyclingedinburgh.info</a>
-<br><a href="http://spokes.org.uk" target="new">Spokes</a> (Lothian Cycle Campaign)
+<li><a href="http://www.hw.ac.uk/cycling" target="new">Heriot-Watt Cycling Club</a></li>
 
-<br><a href="http://www.cycling-edinburgh.org.uk" target="new">Cycling-Edinburgh</a> - details of local rides<br><a href="http://www.ctclothiansandborders.uk/" target="new">CTC Lothians</a> - events and useful info
+<li><a href = "https://www.flickr.com/photos/chdot/14018078361/in/pool-pop2014press/"  target="new"><img src="https://farm8.staticflickr.com/7141/14018078361_8f34c0a2f0_n.jpg" border = "0" width="213" style="padding: 0px;"></a><a href = "http://www.flickr.com/groups/cyclingedinburgh" target="new">
 
+Cycling in Edinburgh Flickr group</a></li>
 
-<br><a href="http://cyclinginfife.wordpress.com">Cycling in Fife</a>
+<li><a href="http://www.laid-back-bikes.scot/" target="new">Laid Back Bikes</a></li>
 
-<br><a href="http://www.ed.ac.uk/schools-departments/transport/cycling" target="new">University of Edinburgh's cycling pages</a> - Lots of useful info for staff and students<br>
-
-<a href="http://www.hw.ac.uk/cycling" target="new">Cycling Development Group at Heriot-Watt University</a>
-
-<br>
-<a href = "https://www.flickr.com/photos/chdot/14018078361/in/pool-pop2014press/"  target="new"><img src="https://farm8.staticflickr.com/7141/14018078361_8f34c0a2f0_n.jpg" border = "0" width="213" style="padding: 0px;"></a><a href = "http://www.flickr.com/groups/cyclingedinburgh" target="new">
-
-Cycling in Edinburgh Flickr group</a></p><a href="http://www.laid-back-bikes.co.uk/" target="new">Laid Back Bikes</a>
-
-<br><a href="http://www.edinburghfalcons.co.uk/" target="new">Edinburgh Falcons</a>
-
-<br><a href="http://www.edinburghfixedgear.co.uk" target="new">Edinburgh Fixed Gear</a>
+<li><a href="http://www.edinburghfalcons.co.uk/" target="new">Edinburgh Falcons</a>
 
 
-<br><a href="http://www.edinburghrc.co.uk/" target="new">Edinburgh RC</a>
+<li><a href="http://www.edinburghrc.co.uk/" target="new">Edinburgh RC</a>
 
 
-<br><a href="http://www.thebikestation.org.uk" target="new">The Bike Station</a>
+<li><a href="http://www.thebikestation.org.uk" target="new">The Bike Station</a>
 
 
-<br><a href="http://cyclingscotland.org" target="new">Cycling Scotland</a>
+<li><a href="http://cyclingscotland.org" target="new">Cycling Scotland</a>
+<li><a href="http://www.sustrans.org.uk/sustrans-near-you/scotland" target="new">Sustrans Scotland</a></li>
 
-<br><a href="http://www.sustrans.org.uk/sustrans-near-you/scotland" target="new">Sustrans Scotland</a>
+<li><a href="http://www.copenhagenize.com">Copenhagenize.com</a> - a vision of what Edinburgh could be like 
+(perhaps)</li>
 
-<br><a href="http://www.copenhagenize.com">Copenhagenize.com</a> - a vision of what Edinburgh could be like 
-(perhaps)
-
-<br><br><a href="http://www.sheldonbrown.com/" target="new">Sheldon Brown</a> -  almost everything you need to know about Bicycles
-
-<br><BR><a href="http://bicycletutor.com" target="new">BicycleTutor</a> - comprehensive DIY guide - with videos!
-
-
-</p>
-</ul>
-<ul>
-
-
+<li><a href="http://www.sheldonbrown.com/" target="new">Sheldon Brown</a> -  almost everything you need to know about Bicycles</li>
 
 <a href="http://extremetracking.com/open?login=cityce"> </a>
-
-<div><img height="1" width="1" alt="" src="http://e2.extreme-dm.com/s11.g?login=cityce&amp;j=n&amp;jv=n" />
+<img height="1" width="1" alt="" src="http://e2.extreme-dm.com/s11.g?login=cityce&amp;j=n&amp;jv=n" />
 </ul>
-
- 
 
 </div>
